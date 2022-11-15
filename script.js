18c8d80ca1f2b16ab963a17a91859891172a556f
alan <alan@iMac-de-alan.local>
2022-11-15 15:27:45 -0600

    Create a div with DOM techniques and an event listener to change the color with the mouse hovering over it

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-let btnAdd = document.querySelector('.canva')
-let containers = document.createElement('div')
-containers.style.backgroundColor = 'white';
-btnAdd.appendChild(containers)
 
+function createGrid(){ 
+let container = document.createElement('div');
+container.classList.add('gridSquares');
+document.getElementById('main').appendChild(container)
+changeToBlack(container)
+}
+
+//cambia de color con el hover
+
+function changeToBlack(container){ 
+container.addEventListener('mouseover', ()=> {
+    container.style.backgroundColor = 'black';
+});
+}
+
+
+function gridSixteen (x){
+    for (i = 0; i < x; i++){
+        createGrid()
+    }
+}
+gridSixteen(16)
